c9ab824fa083c66c0960129a8205934ec82a68fc
Park YongJune <redplus@redplus.net>
2023-03-03 03:06:18 +0900

    Edit Form UI

--- src/VisualAcademy.Desktop/VisualAcademy.Desktop/AppointmentsTypes/EditAppointmentTypeWindow.xaml.cs
+++ src/VisualAcademy.Desktop/VisualAcademy.Desktop/AppointmentsTypes/EditAppointmentTypeWindow.xaml.cs
@@ -1,24 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace VisualAcademy.Desktop.AppointmentsTypes {
     /// <summary>
     /// EditAppointmentTypeWindow.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class EditAppointmentTypeWindow : Window {
-        public EditAppointmentTypeWindow() {
+
+        public string AppointmentTypeName { get; private set; }
+        public new bool IsActive { get; private set; }
+
+        public EditAppointmentTypeWindow(string appointmentTypeName, bool isActive) {
             InitializeComponent();
+            AppointmentTypeName = appointmentTypeName;
+            IsActive = isActive;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e) {
+            AppointmentTypeName = AppointmentTypeNameTextBox.Text;
+            IsActive = (IsActiveCheckBox.IsChecked == true);
+            DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e) {
+            DialogResult = false;
         }
     }
 }
