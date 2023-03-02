1274d780889b5e366549d48a9012e8e562c43e56
Park YongJune <redplus@redplus.net>
2023-03-03 03:38:45 +0900

    모든 데이터를 읽어 출력하는 LoadData 메서드 구현하기

--- src/VisualAcademy.Desktop/VisualAcademy.Desktop/AppointmentsTypes/AppointmentsTypesMainForm.xaml.cs
+++ src/VisualAcademy.Desktop/VisualAcademy.Desktop/AppointmentsTypes/AppointmentsTypesMainForm.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,14 +12,53 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using VisualAcademy.Desktop.Models;
 
 namespace VisualAcademy.Desktop.AppointmentsTypes {
     /// <summary>
     /// AppointmentsTypesMainForm.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class AppointmentsTypesMainForm : Window {
+
+        private readonly List<AppointmentType> _appointmentsTypes;
+
+        private readonly string _connectionString = 
+            "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AppointmentDatabase;Integrated Security=True";
+
         public AppointmentsTypesMainForm() {
             InitializeComponent();
+
+            _appointmentsTypes = new List<AppointmentType>();
+            AppointmentTypesListView.ItemsSource = _appointmentsTypes;
+            LoadData();
+        }
+
+        private void LoadData() {
+            _appointmentsTypes.Clear();
+
+            // ADO.NET을 사용하여 데이터 조회
+            using (var connection = new SqlConnection(_connectionString)) {
+                connection.Open();
+
+                var command = new SqlCommand("SELECT Id, AppointmentTypeName, IsActive FROM AppointmentsTypes", connection);
+
+                using (var reader = command.ExecuteReader()) {
+                    while (reader.Read()) {
+                        var id = (int)reader["Id"];
+                        var name = (string)reader["AppointmentTypeName"];
+                        var isActive = (bool)reader["IsActive"];
+
+                        var appointmentType = new AppointmentType 
+                        { 
+                            Id = id,
+                            AppointmentTypeName = name,
+                            IsActive = isActive
+                        };
+                        _appointmentsTypes.Add(appointmentType);
+                    }
+                }
+            }
+            AppointmentTypesListView.Items.Refresh(); 
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e) {
---
+++ src/VisualAcademy.Desktop/VisualAcademy.Desktop/Models/AppointmentType.cs
@@ -0,0 +1,10 @@
+﻿#nullable disable
+
+namespace VisualAcademy.Desktop.Models {
+    public class AppointmentType {
+        public int Id { get; set; }
+        public string AppointmentTypeName { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
