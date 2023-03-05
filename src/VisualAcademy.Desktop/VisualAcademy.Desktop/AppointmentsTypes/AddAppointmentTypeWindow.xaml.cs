821887d2c2570d4bf79550b08ff1c8746b55a50f
Park YongJune <redplus@redplus.net>
2023-03-05 09:35:34 +0900

    Code Cleanup

--- src/VisualAcademy.Desktop/VisualAcademy.Desktop/AppointmentsTypes/AddAppointmentTypeWindow.xaml.cs
+++ src/VisualAcademy.Desktop/VisualAcademy.Desktop/AppointmentsTypes/AddAppointmentTypeWindow.xaml.cs
@@ -1,35 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#nullable disable
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace VisualAcademy.Desktop.AppointmentsTypes {
-    /// <summary>
-    /// AddAppointmentTypeWindow.xaml에 대한 상호 작용 논리
-    /// </summary>
-    public partial class AddAppointmentTypeWindow : Window {
-        public AddAppointmentTypeWindow() {
-            InitializeComponent();
-        }
+namespace VisualAcademy.Desktop.AppointmentsTypes;
 
-        public string AppointmentTypeName { get; private set; }
+/// <summary>
+/// AddAppointmentTypeWindow.xaml에 대한 상호 작용 논리
+/// </summary>
+public partial class AddAppointmentTypeWindow : Window {
+    public AddAppointmentTypeWindow() => InitializeComponent();
 
-        private void OkButton_Click(object sender, RoutedEventArgs e) {
-            AppointmentTypeName = AppointmentTypeNameTextBox.Text;
-            DialogResult = true; 
-        }
+    public string AppointmentTypeName { get; private set; }
 
-        private void CancelButton_Click(object sender, RoutedEventArgs e) {
-            DialogResult = false;
-        }
+    private void OkButton_Click(object sender, RoutedEventArgs e) {
+        AppointmentTypeName = AppointmentTypeNameTextBox.Text;
+        DialogResult = true;
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e) {
+        DialogResult = false;
     }
 }
