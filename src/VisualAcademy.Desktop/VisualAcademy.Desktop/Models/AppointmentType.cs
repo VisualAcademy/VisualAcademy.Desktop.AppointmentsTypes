ba2adf6840ecf609333e1d13f143bf576f28ce66
Park YongJune <redplus@redplus.net>
2023-03-22 23:03:02 +0900

    Convert to file-scoped namespace

--- src/VisualAcademy.Desktop/VisualAcademy.Desktop/Models/AppointmentType.cs
+++ src/VisualAcademy.Desktop/VisualAcademy.Desktop/Models/AppointmentType.cs
@@ -1,10 +1,9 @@
 ﻿#nullable disable
 
-namespace VisualAcademy.Desktop.Models {
-    public class AppointmentType {
-        public int Id { get; set; }
-        public string AppointmentTypeName { get; set; }
-        public bool IsActive { get; set; } = true;
-        public DateTime DateCreated { get; set; } = DateTime.Now;
-    }
+namespace VisualAcademy.Desktop.Models; 
+public class AppointmentType {
+    public int Id { get; set; }
+    public string AppointmentTypeName { get; set; }
+    public bool IsActive { get; set; } = true;
+    public DateTime DateCreated { get; set; } = DateTime.Now;
 }
