44237986765fbe0c9e9b465fea7ad95ae2d5d3a4
Park YongJune <redplus@redplus.net>
2023-03-29 19:22:56 +0900

    .

--- src/VisualAcademy.Desktop/VisualAcademy.SqlServer/dbo/Stored Procedures/AppointmentsTypes/AppointmentsTypes_Delete.sql
+++ src/VisualAcademy.Desktop/VisualAcademy.SqlServer/dbo/Stored Procedures/AppointmentsTypes/AppointmentsTypes_Delete.sql
@@ -1,7 +1,9 @@
-﻿CREATE PROCEDURE AppointmentsTypes_Delete
-  @Id INT
+﻿-- AppointmentsTypes 테이블에서 특정 Id 값에 해당하는 레코드를 삭제하는 저장 프로시저
+-- @Id: 삭제할 레코드의 Id 값
+CREATE PROCEDURE AppointmentsTypes_Delete
+@Id INT
 AS
 BEGIN
-  DELETE FROM AppointmentsTypes
-  WHERE Id = @Id;
+DELETE FROM AppointmentsTypes
+WHERE Id = @Id;
 END
