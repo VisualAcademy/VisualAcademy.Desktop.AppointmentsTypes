7a07f9cfb6ec73de03a1408c91a97a9c7f95601f
Park YongJune <redplus@redplus.net>
2023-03-31 22:43:27 +0900

    .

--- src/VisualAcademy.Desktop/VisualAcademy.Desktop/AppointmentsTypes/EditAppointmentTypeWindow.xaml.cs
+++ src/VisualAcademy.Desktop/VisualAcademy.Desktop/AppointmentsTypes/EditAppointmentTypeWindow.xaml.cs
@@ -5,22 +5,35 @@ namespace VisualAcademy.Desktop.AppointmentsTypes;
 /// <summary>
 /// EditAppointmentTypeWindow.xaml에 대한 상호 작용 논리
 /// </summary>
-public partial class EditAppointmentTypeWindow : Window {
-
+public partial class EditAppointmentTypeWindow : Window
+{
+    // 약속 유형의 이름을 저장하는 속성
     public string AppointmentTypeName { get; private set; }
+    // 약속 유형의 활성화 상태를 저장하는 속성
     public new bool IsActive { get; private set; }
 
-    public EditAppointmentTypeWindow(string appointmentTypeName, bool isActive) {
+    // 생성자를 사용하여 약속 유형 이름 및 활성화 상태를 초기화
+    public EditAppointmentTypeWindow(string appointmentTypeName, bool isActive)
+    {
         InitializeComponent();
         AppointmentTypeNameTextBox.Text = appointmentTypeName;
         IsActiveCheckBox.IsChecked = isActive;
     }
 
-    private void OkButton_Click(object sender, RoutedEventArgs e) {
+    // 확인 버튼 클릭 시 처리하는 이벤트 핸들러
+    private void OkButton_Click(object sender, RoutedEventArgs e)
+    {
+        // 약속 유형 이름 및 활성화 상태를 저장
         AppointmentTypeName = AppointmentTypeNameTextBox.Text;
         IsActive = (IsActiveCheckBox.IsChecked == true);
+        // 대화 상자 결과를 true로 설정하여 사용자가 확인 버튼을 클릭했음을 나타냄
         DialogResult = true;
     }
 
-    private void CancelButton_Click(object sender, RoutedEventArgs e) => DialogResult = false;
+    // 취소 버튼 클릭 시 처리하는 이벤트 핸들러
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        // 대화 상자 결과를 false로 설정하여 사용자가 취소 버튼을 클릭했음을 나타냄
+        DialogResult = false;
+    }
 }
